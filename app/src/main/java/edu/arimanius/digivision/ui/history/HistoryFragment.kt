025d815397944520852527699332e682b8c37fb5
ahmadsalimi <ahsa9978@gmail.com>
2023-02-07 00:11:52 +0330

    Add grid spacing

--- app/src/main/java/edu/arimanius/digivision/ui/history/HistoryFragment.kt
+++ app/src/main/java/edu/arimanius/digivision/ui/history/HistoryFragment.kt
@@ -1,5 +1,6 @@
 package edu.arimanius.digivision.ui.history
 
+import android.graphics.Rect
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,12 +9,49 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ItemDecoration
 import edu.arimanius.digivision.R
 import edu.arimanius.digivision.databinding.FragmentHistoryBinding
 import edu.arimanius.digivision.ui.search.SearchableFragment
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+
+
+class GridSpacingItemDecoration(
+    private val spanCount: Int,
+    private val spacing: Int,
+    private val includeEdge: Boolean
+) :
+    ItemDecoration() {
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        val position = parent.getChildAdapterPosition(view) // item position
+        val column = position % spanCount // item column
+        if (includeEdge) {
+            outRect.left =
+                spacing - column * spacing / spanCount // spacing - column * ((1f / spanCount) * spacing)
+            outRect.right =
+                (column + 1) * spacing / spanCount // (column + 1) * ((1f / spanCount) * spacing)
+            if (position < spanCount) { // top edge
+                outRect.top = spacing
+            }
+            outRect.bottom = spacing // item bottom
+        } else {
+            outRect.left = column * spacing / spanCount // column * ((1f / spanCount) * spacing)
+            outRect.right =
+                spacing - (column + 1) * spacing / spanCount // spacing - (column + 1) * ((1f /    spanCount) * spacing)
+            if (position >= spanCount) {
+                outRect.top = spacing // item top
+            }
+        }
+    }
+}
 
 class HistoryFragment : SearchableFragment() {
     private var _binding: FragmentHistoryBinding? = null
@@ -34,6 +72,9 @@ class HistoryFragment : SearchableFragment() {
                 else -> GridLayoutManager(context, columnCount)
             }
             adapter = HistoryRecyclerViewAdapter()
+        }
+        if (columnCount > 1) {
+            binding.historyList.addItemDecoration(GridSpacingItemDecoration(columnCount, 3, false))
         }
 
         return binding.root
